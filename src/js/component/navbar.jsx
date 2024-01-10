e6f495962200d69e380d9a83f3cc8399893cb5b8
Ángel Bielsa <145146806+abielsaf@users.noreply.github.com>
2024-01-10 22:44:22 +0000

    Navbar collapse menu working as expected

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -3,9 +3,9 @@ import React from "react";
 
 const Navbar = () => {
     return (
-        <nav className="navbar fixed-top navbar-expand-md navbar-dark bg-dark" >
+        <nav className="navbar fixed-top navbar-expand-md navbar-dark bg-dark">
             <a className="navbar-brand" href="#">Start Bootstrap</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
